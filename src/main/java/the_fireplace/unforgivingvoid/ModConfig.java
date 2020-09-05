136dabfa3aa161dd0c8d6981cedc5f0579cea685
The_Fireplace <the.f1repl4ce@gmail.com>
2020-09-04 23:08:53 -0500

    Finish 1.16.2 rewrite

--- src/main/java/the_fireplace/unforgivingvoid/ModConfig.java
+++ src/main/java/the_fireplace/unforgivingvoid/ModConfig.java
@@ -1,18 +1,33 @@
 package the_fireplace.unforgivingvoid;
 
 import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 import me.sargunvohra.mcmods.autoconfig1u.ConfigData;
 import me.sargunvohra.mcmods.autoconfig1u.annotation.Config;
+import me.sargunvohra.mcmods.autoconfig1u.annotation.ConfigEntry;
 
 import java.util.List;
 
 @Config(name = UnforgivingVoid.MODID)
 public class ModConfig implements ConfigData {
+    @ConfigEntry.Gui.Tooltip
     public int triggerAtY = -32;
-    public int damageOnImpact = 19;
-    public boolean preventImpactDeath = true;
-    public boolean dropObsidian = true;
-    public boolean lavaPlatform = false;
-    public int horizontalDistanceOffset = 512;
+    @ConfigEntry.Gui.Tooltip
+    public boolean dropObsidian = false;
+    @ConfigEntry.Gui.Tooltip(count=2)
+    public int fireResistanceSeconds = 180;
+    @ConfigEntry.Gui.Tooltip
+    public int horizontalDistanceOffset = 128;
+    @ConfigEntry.Gui.Tooltip(count=2)
     public List<String> dimensionFilter = Lists.newArrayList("*");
+
+    @Override
+    public void validatePostLoad() {
+        if(fireResistanceSeconds < 0)
+            fireResistanceSeconds = 0;
+        if(horizontalDistanceOffset < 0)
+            horizontalDistanceOffset = -horizontalDistanceOffset;
+        //Convert to set then list to deduplicate
+        dimensionFilter = Lists.newArrayList(Sets.newHashSet(dimensionFilter));
+    }
 }
--- src/main/java/the_fireplace/unforgivingvoid/mixin/ServerPlayerEntityMixin.java
+++ src/main/java/the_fireplace/unforgivingvoid/mixin/ServerPlayerEntityMixin.java
@@ -1,14 +1,19 @@
 package the_fireplace.unforgivingvoid.mixin;
 
 import com.mojang.authlib.GameProfile;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.RespawnAnchorBlock;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
@@ -18,18 +23,16 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import the_fireplace.unforgivingvoid.UnforgivingVoid;
 
+import java.util.Optional;
 import java.util.Random;
 
 @Mixin(ServerPlayerEntity.class)
 public abstract class ServerPlayerEntityMixin extends PlayerEntity {
-    @Shadow
-    public abstract Entity moveToWorld(ServerWorld destination);
-
     public ServerPlayerEntityMixin(World world, BlockPos pos, float yaw, GameProfile profile) {
         super(world, pos, yaw, profile);
     }
 
-    @Inject(at = @At(value="TAIL"), method = "tick")
+    @Inject(at = @At(value="TAIL"), method = "playerTick")
     private void tick(CallbackInfo callbackInfo) {
         if(!this.world.isClient() && this.getBlockPos().getY() <= UnforgivingVoid.config.triggerAtY) {
             MinecraftServer server = getServer();
@@ -42,10 +45,21 @@ public abstract class ServerPlayerEntityMixin extends PlayerEntity {
                     }
                 if (doTeleport) {
                     addStatusEffect(new StatusEffectInstance(StatusEffects.BLINDNESS, 60, 3));
-                    //Random rand = world.random;
-                    //BlockPos spawnPos = new BlockPos(getBlockPos().getX() / 8 - UnforgivingVoid.config.horizontalDistanceOffset + rand.nextInt(UnforgivingVoid.config.horizontalDistanceOffset * 2), rand.nextInt(100) + 16, getBlockPos().getZ() / 8 - UnforgivingVoid.config.horizontalDistanceOffset + rand.nextInt(UnforgivingVoid.config.horizontalDistanceOffset * 2));
-                    moveToWorld(server.getWorld(World.NETHER));
-                    //teleport(server.getWorld(World.NETHER), spawnPos.getX(), spawnPos.getY(), spawnPos.getZ(), yaw, pitch);
+                    Random rand = world.random;
+                    ServerWorld nether = server.getWorld(World.NETHER);
+                    assert nether != null;
+                    Optional<Vec3d> spawnVec;
+                    do {
+                        spawnVec = RespawnAnchorBlock.findRespawnPosition(this.getType(), nether, new BlockPos(getBlockPos().getX() / 8 - UnforgivingVoid.config.horizontalDistanceOffset + rand.nextInt(UnforgivingVoid.config.horizontalDistanceOffset * 2), rand.nextInt(100) + 16, getBlockPos().getZ() / 8 - UnforgivingVoid.config.horizontalDistanceOffset + rand.nextInt(UnforgivingVoid.config.horizontalDistanceOffset * 2)));
+                    } while(!spawnVec.isPresent());
+                    BlockPos spawnPos = new BlockPos(spawnVec.get());
+                    //Make sure the chunk is created BEFORE teleporting the player, or else we end up with the infinite derp loop
+                    nether.getChunk(spawnPos);
+                    if(UnforgivingVoid.config.fireResistanceSeconds > 0)
+                        addStatusEffect(new StatusEffectInstance(StatusEffects.FIRE_RESISTANCE, UnforgivingVoid.config.fireResistanceSeconds*20));
+                    ((ServerPlayerEntity)(Object)this).teleport(nether, spawnPos.getX(), spawnPos.getY(), spawnPos.getZ(), yaw, pitch);
+                    if(UnforgivingVoid.config.dropObsidian)
+                        nether.spawnEntity(new ItemEntity(nether, spawnPos.getX(), spawnPos.getY(), spawnPos.getZ(), new ItemStack(Blocks.OBSIDIAN, 14)));
                 }
             }
         }
