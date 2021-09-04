e5ee01e208b4fe56f9bf941643c7f4ef43f17f11
The_Fireplace <the.f1repl4ce@gmail.com>
2021-09-04 15:13:00 -0500

    Refactor transfer logic

--- src/main/java/the_fireplace/unforgivingvoid/VoidTransfer.java
+++ src/main/java/the_fireplace/unforgivingvoid/VoidTransfer.java
@@ -14,6 +14,7 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.World;
 import the_fireplace.unforgivingvoid.config.DimensionConfig;
 import the_fireplace.unforgivingvoid.config.DimensionConfigManager;
 
@@ -34,29 +35,51 @@ public final class VoidTransfer {
         ServerWorld world = serverPlayerEntity.getServerWorld();
         DimensionConfig dimensionConfig = dimensionConfigManager.getSettings(world.getRegistryKey().getValue());
 
-        serverPlayerEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.BLINDNESS, 60, 3));
-        Random rand = world.random;
-
-        ServerWorld targetWorld = server.getWorld(RegistryKey.of(Registry.WORLD_KEY, new Identifier(dimensionConfig.getTargetDimension())));
+        RegistryKey<World> targetWorldRegistryKey = createTargetWorldRegistryKey(dimensionConfig);
+        ServerWorld targetWorld = server.getWorld(targetWorldRegistryKey);
         if (targetWorld == null) {
             UnforgivingVoid.getLogger().error("Target world not found: " + dimensionConfig.getTargetDimension());
             return;
         }
-        Optional<Vec3d> spawnVec;
         BlockPos playerPos = serverPlayerEntity.getBlockPos();
+        BlockPos spawnPos = findSpawnPosition(serverPlayerEntity, dimensionConfig, targetWorld, playerPos);
+
+        preloadTargetChunk(targetWorld, spawnPos);
+        applyStatusEffects(serverPlayerEntity, dimensionConfig);
+        serverPlayerEntity.teleport(targetWorld, spawnPos.getX(), spawnPos.getY(), spawnPos.getZ(), serverPlayerEntity.yaw, serverPlayerEntity.pitch);
+        createAssistanceMaterials(dimensionConfig, targetWorld, spawnPos);
+    }
+
+    private RegistryKey<World> createTargetWorldRegistryKey(DimensionConfig dimensionConfig) {
+        return RegistryKey.of(Registry.WORLD_KEY, new Identifier(dimensionConfig.getTargetDimension()));
+    }
+
+    private BlockPos findSpawnPosition(ServerPlayerEntity serverPlayerEntity, DimensionConfig dimensionConfig, ServerWorld targetWorld, BlockPos playerPos) {
+        Optional<Vec3d> spawnVec;
+        Random rand = targetWorld.getRandom();
         do {
             int targetX = playerPos.getX() / 8 - dimensionConfig.getHorizontalDistanceOffset() + rand.nextInt(dimensionConfig.getHorizontalDistanceOffset() * 2);
             int targetY = rand.nextInt(100) + 16;
             int targetZ = playerPos.getZ() / 8 - dimensionConfig.getHorizontalDistanceOffset() + rand.nextInt(dimensionConfig.getHorizontalDistanceOffset() * 2);
             spawnVec = RespawnAnchorBlock.findRespawnPosition(serverPlayerEntity.getType(), targetWorld, new BlockPos(targetX, targetY, targetZ));
         } while (!spawnVec.isPresent());
-        BlockPos spawnPos = new BlockPos(spawnVec.get());
+
+        return new BlockPos(spawnVec.get());
+    }
+
+    private void preloadTargetChunk(ServerWorld targetWorld, BlockPos spawnPos) {
         //Make sure the chunk is created BEFORE teleporting the player, or else we end up with the infinite derp loop
         targetWorld.getChunk(spawnPos);
+    }
+
+    private void applyStatusEffects(ServerPlayerEntity serverPlayerEntity, DimensionConfig dimensionConfig) {
+        serverPlayerEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.BLINDNESS, 60, 3));
         if (dimensionConfig.getFireResistanceSeconds() > 0) {
             serverPlayerEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.FIRE_RESISTANCE, dimensionConfig.getFireResistanceSeconds() * 20));
         }
-        serverPlayerEntity.teleport((ServerWorld) targetWorld, spawnPos.getX(), spawnPos.getY(), spawnPos.getZ(), serverPlayerEntity.yaw, serverPlayerEntity.pitch);
+    }
+
+    private void createAssistanceMaterials(DimensionConfig dimensionConfig, ServerWorld targetWorld, BlockPos spawnPos) {
         if (dimensionConfig.isDropObsidian()) {
             targetWorld.spawnEntity(new ItemEntity(targetWorld, spawnPos.getX(), spawnPos.getY(), spawnPos.getZ(), new ItemStack(Blocks.OBSIDIAN, 14)));
         }
