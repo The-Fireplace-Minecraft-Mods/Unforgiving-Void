60420a0b5334d3bd061f34569c18780530a1263a
The_Fireplace <the.f1repl4ce@gmail.com>
2021-09-04 13:44:09 -0500

    Get things compiling with the new config system

---
+++ src/main/java/the_fireplace/unforgivingvoid/VoidTransfer.java
@@ -0,0 +1,64 @@
+package the_fireplace.unforgivingvoid;
+
+import net.minecraft.block.Blocks;
+import net.minecraft.block.RespawnAnchorBlock;
+import net.minecraft.entity.ItemEntity;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.world.World;
+import the_fireplace.unforgivingvoid.config.DimensionConfig;
+import the_fireplace.unforgivingvoid.config.DimensionConfigManager;
+
+import javax.inject.Inject;
+import java.util.Optional;
+import java.util.Random;
+
+public final class VoidTransfer {
+
+    private final DimensionConfigManager dimensionConfigManager;
+
+    @Inject
+    public VoidTransfer(DimensionConfigManager dimensionConfigManager) {
+        this.dimensionConfigManager = dimensionConfigManager;
+    }
+
+    public void initiateVoidTransfer(ServerPlayerEntity serverPlayerEntity, MinecraftServer server) {
+        ServerWorld world = serverPlayerEntity.getServerWorld();
+        DimensionConfig dimensionConfig = dimensionConfigManager.getSettings(world.getRegistryKey().getValue());
+
+        serverPlayerEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.BLINDNESS, 60, 3));
+        Random rand = world.random;
+
+        World targetWorld = server.getRegistryManager().get(Registry.WORLD_KEY).get(new Identifier(dimensionConfig.getTargetDimension()));
+        if (!(targetWorld instanceof ServerWorld)) {
+            UnforgivingVoid.getLogger().error("Target world not found: " + dimensionConfig.getTargetDimension());
+            return;
+        }
+        Optional<Vec3d> spawnVec;
+        BlockPos playerPos = serverPlayerEntity.getBlockPos();
+        do {
+            int targetX = playerPos.getX() / 8 - dimensionConfig.getHorizontalDistanceOffset() + rand.nextInt(dimensionConfig.getHorizontalDistanceOffset() * 2);
+            int targetY = rand.nextInt(100) + 16;
+            int targetZ = playerPos.getZ() / 8 - dimensionConfig.getHorizontalDistanceOffset() + rand.nextInt(dimensionConfig.getHorizontalDistanceOffset() * 2);
+            spawnVec = RespawnAnchorBlock.findRespawnPosition(serverPlayerEntity.getType(), targetWorld, new BlockPos(targetX, targetY, targetZ));
+        } while (!spawnVec.isPresent());
+        BlockPos spawnPos = new BlockPos(spawnVec.get());
+        //Make sure the chunk is created BEFORE teleporting the player, or else we end up with the infinite derp loop
+        targetWorld.getChunk(spawnPos);
+        if (dimensionConfig.getFireResistanceSeconds() > 0) {
+            serverPlayerEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.FIRE_RESISTANCE, dimensionConfig.getFireResistanceSeconds() * 20));
+        }
+        serverPlayerEntity.teleport((ServerWorld) targetWorld, spawnPos.getX(), spawnPos.getY(), spawnPos.getZ(), serverPlayerEntity.yaw, serverPlayerEntity.pitch);
+        if (dimensionConfig.isDropObsidian()) {
+            targetWorld.spawnEntity(new ItemEntity(targetWorld, spawnPos.getX(), spawnPos.getY(), spawnPos.getZ(), new ItemStack(Blocks.OBSIDIAN, 14)));
+        }
+    }
+}
--- src/main/java/the_fireplace/unforgivingvoid/mixin/ServerPlayerEntityMixin.java
+++ src/main/java/the_fireplace/unforgivingvoid/mixin/ServerPlayerEntityMixin.java
@@ -1,64 +1,42 @@
 package the_fireplace.unforgivingvoid.mixin;
 
 import com.mojang.authlib.GameProfile;
-import net.minecraft.block.Blocks;
-import net.minecraft.block.RespawnAnchorBlock;
-import net.minecraft.entity.ItemEntity;
-import net.minecraft.entity.effect.StatusEffectInstance;
-import net.minecraft.entity.effect.StatusEffects;
+import dev.the_fireplace.annotateddi.api.DIContainer;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import the_fireplace.unforgivingvoid.UnforgivingVoid;
-
-import java.util.Optional;
-import java.util.Random;
+import the_fireplace.unforgivingvoid.VoidTransfer;
+import the_fireplace.unforgivingvoid.config.DimensionConfig;
+import the_fireplace.unforgivingvoid.config.DimensionConfigManager;
 
 @Mixin(ServerPlayerEntity.class)
 public abstract class ServerPlayerEntityMixin extends PlayerEntity {
+
     protected ServerPlayerEntityMixin(World world, BlockPos pos, float yaw, GameProfile profile) {
         super(world, pos, yaw, profile);
     }
 
     @Inject(at = @At(value="TAIL"), method = "playerTick")
     private void tick(CallbackInfo callbackInfo) {
-        if(!this.world.isClient() && this.getBlockPos().getY() <= UnforgivingVoid.config.getTriggerAtY()) {
+        DimensionConfig dimensionConfig = DIContainer.get().getInstance(DimensionConfigManager.class).getSettings(this.world.getRegistryKey().getValue());
+        if (!this.world.isClient()
+            && dimensionConfig.isEnabled()
+            && this.getBlockPos().getY() <= getBottomY(world) - dimensionConfig.getTriggerDistance()
+        ) {
             MinecraftServer server = getServer();
-            if(server != null) {
-                boolean doTeleport = UnforgivingVoid.config.dimensionFilter.contains("*");
-                for (String dim : UnforgivingVoid.config.dimensionFilter)
-                    if (!"*".equals(dim) && world.getRegistryKey().getValue().toString().equalsIgnoreCase(dim)) {
-                        doTeleport = !doTeleport;
-                        break;
-                    }
-                if (doTeleport) {
-                    addStatusEffect(new StatusEffectInstance(StatusEffects.BLINDNESS, 60, 3));
-                    Random rand = world.random;
-                    ServerWorld nether = server.getWorld(World.NETHER);
-                    assert nether != null;
-                    Optional<Vec3d> spawnVec;
-                    do {
-                        spawnVec = RespawnAnchorBlock.findRespawnPosition(this.getType(), nether, new BlockPos(getBlockPos().getX() / 8 - UnforgivingVoid.config.getHorizontalDistanceOffset() + rand.nextInt(UnforgivingVoid.config.getHorizontalDistanceOffset() * 2), rand.nextInt(100) + 16, getBlockPos().getZ() / 8 - UnforgivingVoid.config.getHorizontalDistanceOffset() + rand.nextInt(UnforgivingVoid.config.getHorizontalDistanceOffset() * 2)));
-                    } while(!spawnVec.isPresent());
-                    BlockPos spawnPos = new BlockPos(spawnVec.get());
-                    //Make sure the chunk is created BEFORE teleporting the player, or else we end up with the infinite derp loop
-                    nether.getChunk(spawnPos);
-                    if(UnforgivingVoid.config.getFireResistanceSeconds() > 0)
-                        addStatusEffect(new StatusEffectInstance(StatusEffects.FIRE_RESISTANCE, UnforgivingVoid.config.getFireResistanceSeconds() *20));
-                    ((ServerPlayerEntity)(Object)this).teleport(nether, spawnPos.getX(), spawnPos.getY(), spawnPos.getZ(), yaw, pitch);
-                    if(UnforgivingVoid.config.isDropObsidian())
-                        nether.spawnEntity(new ItemEntity(nether, spawnPos.getX(), spawnPos.getY(), spawnPos.getZ(), new ItemStack(Blocks.OBSIDIAN, 14)));
-                }
+            if (server != null) {
+                DIContainer.get().getInstance(VoidTransfer.class).initiateVoidTransfer((ServerPlayerEntity) (Object) this, server);
             }
         }
+    }
+
+    private int getBottomY(World world) {
+        return 0;
     }
 }
