d5ce46f65c67395f43fd0ac06061f58eb864886b
The_Fireplace <the.f1repl4ce@gmail.com>
2021-09-05 10:35:43 -0500

    1.15.2 backport - collision checking

--- src/main/java/dev/the_fireplace/unforgivingvoid/usecase/SpawnPositionLocator.java
+++ src/main/java/dev/the_fireplace/unforgivingvoid/usecase/SpawnPositionLocator.java
@@ -3,18 +3,18 @@ package dev.the_fireplace.unforgivingvoid.usecase;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.UnmodifiableIterator;
 import dev.the_fireplace.unforgivingvoid.UnforgivingVoidConstants;
-import net.minecraft.block.AirBlock;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
-import net.minecraft.block.CampfireBlock;
-import net.minecraft.entity.Entity;
+import net.minecraft.block.*;
+import net.minecraft.entity.EntityDimensions;
 import net.minecraft.entity.EntityType;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.math.Vec3i;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
 import net.minecraft.world.Heightmap;
 import net.minecraft.world.dimension.DimensionType;
 import org.jetbrains.annotations.Nullable;
@@ -152,7 +152,7 @@ public final class SpawnPositionLocator {
     }
 
     /**
-     * Backported replacement for 1.16.5 method BlockRespawnAnchor.findRespawnPosition
+     * Backport replacement for 1.16.5 method BlockRespawnAnchor.findRespawnPosition
      */
     private Optional<Vec3d> findRespawnPosition(EntityType<?> entity, ServerWorld world, BlockPos pos) {
         Optional<Vec3d> optional = findSafeNearbyPosition(entity, world, pos, true);
@@ -160,7 +160,7 @@ public final class SpawnPositionLocator {
     }
 
     /**
-     * Backported replacement for 1.16.5 method BlockRespawnAnchor.method_
+     * Backport replacement for 1.16.5 method BlockRespawnAnchor.method_
      */
     private Optional<Vec3d> findSafeNearbyPosition(EntityType<?> entityType, ServerWorld world, BlockPos blockPos, boolean protectAgainstHazardousSpawn) {
         BlockPos.Mutable mutableTargetPos = new BlockPos.Mutable();
@@ -181,24 +181,59 @@ public final class SpawnPositionLocator {
     }
 
     /**
-     * Backported replacement for 1.16.5 method Dismounting.method_30769
+     * Backport replacement for 1.16.5 method Dismounting.method_30769
      */
     @Nullable
     private Vec3d checkPositionForSafety(EntityType<?> entityType, ServerWorld world, BlockPos blockPos, boolean protectAgainstHazardousSpawn) {
         if (protectAgainstHazardousSpawn && isHazardousSpawnPosition(entityType, world.getBlockState(blockPos))) {
             return null;
         }
-        double d = world.getDismountHeight(getCollisionShape(world, blockPos), () -> {
-            return getCollisionShape(world, blockPos.down());
-        });
-        if (!canDismountInBlock(d)) {
+        double dismountHeight = world.getDismountHeight(getCollisionShape(world, blockPos), () -> getCollisionShape(world, blockPos.down()));
+        if (!canDismountInBlock(dismountHeight)) {
             return null;
-        } else if (protectAgainstHazardousSpawn && d <= 0.0D && isHazardousSpawnPosition(entityType, world.getBlockState(blockPos.down()))) {
+        } else if (protectAgainstHazardousSpawn && dismountHeight <= 0.0D && isHazardousSpawnPosition(entityType, world.getBlockState(blockPos.down()))) {
             return null;
         } else {
-            Vec3d vec3d = Vec3d.ofCenter(blockPos, d);
-            return world.getBlockCollisions((Entity) null, entityType.getDimensions().getBoxAt(vec3d)).allMatch(VoxelShape::isEmpty) ? vec3d : null;
+            Vec3d vec3d = getCenteredSpawnVector(blockPos, dismountHeight);
+            return world.getBlockCollisions(null, getCollisionBoxForPosition(entityType.getDimensions(), vec3d.getX(), vec3d.getY(), vec3d.getZ()))
+                .allMatch(VoxelShape::isEmpty)
+                ? vec3d
+                : null;
         }
+    }
+
+    /**
+     * Backport replacement for 1.16.5 method Dismounting.getCollisionShape
+     */
+    private VoxelShape getCollisionShape(BlockView world, BlockPos pos) {
+        BlockState blockState = world.getBlockState(pos);
+        return !(blockState.getBlock() instanceof LadderBlock || blockState.getBlock() instanceof VineBlock)
+            && (!(blockState.getBlock() instanceof TrapdoorBlock) || !(Boolean) blockState.get(TrapdoorBlock.OPEN))
+            ? blockState.getCollisionShape(world, pos)
+            : VoxelShapes.empty();
+    }
+
+    /**
+     * Backport replacement for 1.16.5 method Dismounting.canDismountInBlock
+     */
+    private boolean canDismountInBlock(double height) {
+        return !Double.isInfinite(height) && height < 1.0D;
+    }
+
+    /**
+     * Backport replacement for 1.16.5 method Vec3d.ofCenter
+     */
+    private Vec3d getCenteredSpawnVector(Vec3i vec, double deltaY) {
+        return new Vec3d((double) vec.getX() + 0.5D, (double) vec.getY() + deltaY, (double) vec.getZ() + 0.5D);
+    }
+
+    /**
+     * Backport replacement for 1.16.5 method EntityDimensions.getBoxAt
+     */
+    public Box getCollisionBoxForPosition(EntityDimensions entityDimensions, double x, double y, double z) {
+        float widthOffset = entityDimensions.width / 2.0F;
+        float heightOffset = entityDimensions.height;
+        return new Box(x - (double) widthOffset, y, z - (double) widthOffset, x + (double) widthOffset, y + (double) heightOffset, z + (double) widthOffset);
     }
 
     private boolean isSafeSky(EntityType<?> entityType, ServerWorld targetWorld, BlockPos blockPos) {
@@ -206,7 +241,7 @@ public final class SpawnPositionLocator {
     }
 
     /**
-     * Backported replacement for 1.16.5 method EntityType#isInvalidSpawn
+     * Backport replacement for 1.16.5 method EntityType#isInvalidSpawn
      */
     private boolean isHazardousSpawnPosition(EntityType<?> entityType, BlockState state) {
         //This first if statement isn't quite equivalent to the one from 1.16+ EntityType, but should be good enough for sky spawning purposes
