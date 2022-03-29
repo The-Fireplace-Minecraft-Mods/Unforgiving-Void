6d2729ed2203bbb8feaf0d4ce05b26267aa4afad
The_Fireplace <git@the-fireplace.dev>
2022-03-28 19:20:55 -0500

    Optimize player lock to help prevent concurrent teleportation
    Add debug logging to help identify issues with teleporting

--- src/main/java/dev/the_fireplace/unforgivingvoid/mixin/ServerPlayerEntityMixin.java
+++ src/main/java/dev/the_fireplace/unforgivingvoid/mixin/ServerPlayerEntityMixin.java
@@ -2,15 +2,18 @@ package dev.the_fireplace.unforgivingvoid.mixin;
 
 import com.mojang.authlib.GameProfile;
 import dev.the_fireplace.annotateddi.api.DIContainer;
+import dev.the_fireplace.unforgivingvoid.UnforgivingVoidConstants;
 import dev.the_fireplace.unforgivingvoid.config.DimensionConfig;
 import dev.the_fireplace.unforgivingvoid.config.DimensionConfigManager;
 import dev.the_fireplace.unforgivingvoid.usecase.QueueVoidTransfer;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -18,6 +21,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ServerPlayerEntity.class)
 public abstract class ServerPlayerEntityMixin extends PlayerEntity
 {
+    @Shadow
+    public abstract ServerWorld getWorld();
 
     protected ServerPlayerEntityMixin(World world, BlockPos pos, float yaw, GameProfile profile) {
         super(world, pos, yaw, profile);
@@ -32,6 +37,11 @@ public abstract class ServerPlayerEntityMixin extends PlayerEntity
         ) {
             MinecraftServer server = getServer();
             if (server != null) {
+                UnforgivingVoidConstants.getLogger().debug(
+                    "Player is below the minimum height. Teleporting to new dimension. Current position is {}, and current world is {}",
+                    getBlockPos().toShortString(),
+                    getWorld().getRegistryKey().getValue()
+                );
                 DIContainer.get().getInstance(QueueVoidTransfer.class).queueTransfer((ServerPlayerEntity) (Object) this, server);
             }
         }
