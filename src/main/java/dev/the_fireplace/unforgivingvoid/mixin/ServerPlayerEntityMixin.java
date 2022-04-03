850f047efb433606af0dc2bd3ff83f5035f7f8aa
solonovamax <solonovamax@12oclockpoint.com>
2022-04-03 17:27:13 -0400

    Fix repeatedly teleporting when server hasn't received client packet
    Signed-off-by: solonovamax <solonovamax@12oclockpoint.com>

--- src/main/java/dev/the_fireplace/unforgivingvoid/mixin/ServerPlayerEntityMixin.java
+++ src/main/java/dev/the_fireplace/unforgivingvoid/mixin/ServerPlayerEntityMixin.java
@@ -1,5 +1,6 @@
 package dev.the_fireplace.unforgivingvoid.mixin;
 
+
 import com.mojang.authlib.GameProfile;
 import dev.the_fireplace.annotateddi.api.DIContainer;
 import dev.the_fireplace.unforgivingvoid.UnforgivingVoidConstants;
@@ -18,40 +19,44 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+
+@SuppressWarnings("AbstractClassNeverImplemented")
 @Mixin(ServerPlayerEntity.class)
-public abstract class ServerPlayerEntityMixin extends PlayerEntity
-{
+public abstract class ServerPlayerEntityMixin extends PlayerEntity {
+    
     @Shadow
-    public abstract ServerWorld getServerWorld();
-
-    @Shadow
-    public abstract boolean isInTeleportationState();
-
+    private boolean inTeleportationState;
+    
     protected ServerPlayerEntityMixin(World world, BlockPos pos, float yaw, GameProfile profile) {
         super(world, pos, yaw, profile);
     }
-
+    
     @Inject(at = @At("TAIL"), method = "playerTick")
     private void tick(CallbackInfo callbackInfo) {
         DimensionConfig dimensionConfig = DIContainer.get().getInstance(DimensionConfigManager.class).getSettings(this.world.getRegistryKey().getValue());
         if (!this.world.isClient()
             && dimensionConfig.isEnabled()
-            && this.getBlockPos().getY() <= getBottomY(world) - dimensionConfig.getTriggerDistance()
+            && this.getBlockPos().getY() <= world.getBottomY() - dimensionConfig.getTriggerDistance()
             && !isInTeleportationState()
         ) {
             MinecraftServer server = getServer();
             if (server != null) {
                 UnforgivingVoidConstants.getLogger().debug(
-                    "Player is below the minimum height. Teleporting to new dimension. Current position is {}, and current world is {}",
-                    getBlockPos().toShortString(),
-                    getServerWorld().getRegistryKey().getValue()
-                );
+                        "Player is below the minimum height. Teleporting to new dimension. Current position is {}, and current world is {}",
+                        getBlockPos().toShortString(),
+                        getServerWorld().getRegistryKey().getValue()
+                                                          );
+                
+                inTeleportationState = true;
+                
                 DIContainer.get().getInstance(QueueVoidTransfer.class).queueTransfer((ServerPlayerEntity) (Object) this, server);
             }
         }
     }
-
-    private int getBottomY(World world) {
-        return world.getBottomY();
-    }
+    
+    @Shadow
+    public abstract ServerWorld getServerWorld();
+    
+    @Shadow
+    public abstract boolean isInTeleportationState();
 }
